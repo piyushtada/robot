71392a527628f11e1f637eb51662d0aa6777ee1d
piyushtada <piyushtada@gmail.com>
2023-01-15 11:10:18 +0100

    added comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+// react is the main engine that runs and do all the things for us.
 import React from 'react';
+// here is the tool set that lets us react with the virtual dom that react creates.
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// we are selecting the root from our public/index.html and updating 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// this render function is creating the app we see on the server
 root.render(
+  // strictMode what it does is use only the features available in the latest version 
+  // we don't use depricated things
+  // app is the whole app we are seeing in the browser
   <React.StrictMode>
     <App />
   </React.StrictMode>
