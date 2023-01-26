f0fd66e35ed483bc6a817fe3c4018c5ad37c89f3
piyushtada <piyushtada@gmail.com>
2023-01-26 09:22:56 +0100

    search half implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // we need to import componet because we want to use classes
 import { Component} from 'react';
 // this is loading our logo
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // this is getting the css related to the app
 import './App.css';
 
@@ -12,18 +12,50 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: 'piyush'
+      monsters: [],
     }
   }
-
+  // get data from api before the component is rendered
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => {
+      this.setState(
+        () => {
+          return {monsters: users};
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    })
+  }
+  // explain this function
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-         <h1> {this.state.name}</h1>
-         <button> Change name</button>
-        </header>
+        <input className='search-box' type='text' placeholder='search monsters' onChange={
+          (event) => {
+            // here we get the search string
+            const searchString = event.target.value.toLocaleLowerCase();
+            // we use the search string to filter our moster list
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return {monsters: filteredMonsters};
+            })
+          }} />
+
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+              <p>{monster.email}</p>
+            </div>
+          );
+        })}
       </div>
     );
 
